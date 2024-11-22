80c6584b3da955e9ea67d423c61d3a73b8190b33
JuanJTorresB <juanjtorresbecerra@gmail.com>
2024-11-22 07:05:21 -0500

    feat: :sparkles: New Projects Added

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -6,10 +6,10 @@ export const ProjectCard = ({ name, imgUbi, description, Icons, Href }) => {
 
     return <article className="bg-amber-100 bg-opacity-10 border border-slate-50 backdrop-blur-lg flex w-[966px] p-14 rounded-[60px] gap-x-7 items-center h-full">
         <a href={Href} target="_blank" className="flex justify-center items-center gap-8">
-            <div className={"flex flex-col text-amberMine h-full justify-center"}>
+            <div className={"flex flex-col text-amberMine h-full justify-center max-w-[390px]"}>
                 <h6 className={generalLetras + " font-bold text-[32px] text-nowrap"}>{name}</h6>
                 <p className={generalLetras + " text-justify tracking-wide font-extralight text-2xl leading-loose pt-5 text-slate-50"}>{description}</p>
-                <ul className="flex gap-x-6  pt-5">
+                <ul className="flex flex-wrap gap-6 pt-5 justify-center items-center">
                     {Icons.map((e) => {
                         return (<li key={contador++}>
                             {e}
